1ee6d699894f37f4309ceaa96b8e059050bd3cb7
Russ Ferriday <russf@topia.com>
2014-07-30 15:08:53 +0100

    Add case independence, consonant counting. Rename. Tidy.

--- letters.py
+++ letters.py
@@ -1,9 +1,22 @@
-def vowel_frequency(s):
-    """return dict of vowel frequency
+import re
+
+VOWELS = 'aeiou'
+CONS = 'bcdfghjklmnpqrstvwxyz'
+
+def vowel_frequency_consonant_sum(s):
+    """return dict of vowel frequency and sum of all consonants
        assumes ASCII only.
-       TODO: refine spec and requirements re: i18n.
+       assumes case independent.
+       TODO: check spec and requirements re: i18n and case
     """
+    s = s.lower()
+
     stats = {}
-    for v in 'aeiou':
+
+    for v in VOWELS:
         stats[v] = s.count(v)
+
+    cons_re = re.compile("[%s]" % CONS)
+    stats['consonants'] = len(cons_re.findall(s))
+
     return stats
