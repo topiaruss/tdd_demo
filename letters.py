f79c294db0e10dcf72f7096b66986a4327e80f2b
Russ Ferriday <russf@topia.com>
2014-07-30 14:43:26 +0100

    Fix, add vowel tests. Add failing consonant test.

--- letters.py
+++ letters.py
@@ -3,5 +3,7 @@ def vowel_frequency(s):
        assumes ASCII only.
        TODO: refine spec and requirements re: i18n.
     """
-    ret = dict(a=0, e=0, i=0, o=0, u=0)
-    return ret
+    stats = {}
+    for v in 'aeiou':
+        stats[v] = s.count(v)
+    return stats
--- test.py
+++ test.py
@@ -1,9 +1,6 @@
 import unittest
 
-class TestSequenceFunctions(unittest.TestCase):
-
-    def setUp(self):
-        pass
+class TestLetterCounting(unittest.TestCase):
 
     def test_empty_string_has_zero_occurrences(self):
         import letters
@@ -11,7 +8,15 @@ class TestSequenceFunctions(unittest.TestCase):
 
     def test_simple_vowel_counts(self):
         import letters
-        self.assertEqual(dict(a=0, e=1, i=2, o=3, u=4), letters.vowel_frequency('eiiooouuu'))
+        self.assertEqual(dict(a=0, e=1, i=2, o=3, u=4), letters.vowel_frequency('eiiooouuuu'))
+
+    def test_counts_unaffected_by_other_characters(self):
+        import letters
+        self.assertEqual(dict(a=0, e=1, i=2, o=3, u=4), letters.vowel_frequency('eiiooouuuu_+1234567890!@#$%^&*()bcd'))
+
+    def test_simple_consonant_counting(self):
+        import letters
+        self.assertEqual(dict(a=0, e=0, i=0, o=0, u=0, consonants=1), letters.vowel_frequency('b'))
 
 if __name__ == '__main__':
     unittest.main()
