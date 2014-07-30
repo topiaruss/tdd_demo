1537eca38aba2a9bc304930a86d6c41e030e6976
Russ Ferriday <russf@topia.com>
2014-07-30 14:28:22 +0100

    Test empty string. Add failing simple count test.

---
+++ letters.py
@@ -0,0 +1,7 @@
+def vowel_frequency(s):
+    """return dict of vowel frequency
+       assumes ASCII only.
+       TODO: refine spec and requirements re: i18n.
+    """
+    ret = dict(a=0, e=0, i=0, o=0, u=0)
+    return ret
--- test.py
+++ test.py
@@ -5,9 +5,13 @@ class TestSequenceFunctions(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_null(self):
-        """just get test infra in place."""
-        pass
+    def test_empty_string_has_zero_occurrences(self):
+        import letters
+        self.assertEqual(dict(a=0, e=0, i=0, o=0, u=0), letters.vowel_frequency(''))
+
+    def test_simple_vowel_counts(self):
+        import letters
+        self.assertEqual(dict(a=0, e=1, i=2, o=3, u=4), letters.vowel_frequency('eiiooouuu'))
 
 if __name__ == '__main__':
     unittest.main()
