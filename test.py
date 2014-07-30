6e199dd9a34006c65241ccf7d9f2f34fa1e23cef
Russ Ferriday <russf@topia.com>
2014-07-30 14:13:06 +0100

    Create a test module, and make it pass.

---
+++ test.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_null(self):
+        """just get test infra in place."""
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
